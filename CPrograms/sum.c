b109a90f3ceb45d2b5c7dd69e0649f16c6a22fef
GustavoViniciusdeMorais <gustavocop14dos@gmail.com>
2024-07-29 17:01:56 -0300

    - Added the C and assembly code to sum two numbers. Removed old compiled c files not necessaries

---
+++ CPrograms/sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    sum = num1 + num2;
+
+    printf("%d + %d = %d\n", num1, num2, sum);
+
+    return 0;
+}
