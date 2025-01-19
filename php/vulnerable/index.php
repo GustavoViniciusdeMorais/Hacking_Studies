f82267831f630b65b1ab056922eddff89dd66d0d
GustavoViniciusdeMorais <gustavocop14dos@gmail.com>
2025-01-19 10:25:45 -0300

    - added protected query version

--- php/vulnerable/index.php
+++ php/vulnerable/index.php
@@ -1,15 +1,30 @@
 <?php
 
 try{
+    $protect = false;
     $user='dev';
     $pass='1234';
     $dbh = new \PDO('mysql:host=localhost;dbname=api', $user, $pass);
 
     $id = $_GET['id'];
-    echo "Param: " . $id . "\n";
-    $vulnerableSql = "SELECT * FROM customers WHERE id = $id";
-    foreach ($dbh->query($vulnerableSql) as $row) {
-        echo "Name: " . $row['name'] . "\n";
+
+    if (empty($id)) {
+        $id = 1;
+    }
+
+    if ($protect) {
+        $sql = "SELECT * FROM customers WHERE id = :id";
+        $sth = $dbh->prepare($sql, [\PDO::ATTR_CURSOR => \PDO::CURSOR_FWDONLY]);
+        $sth->execute(['id' => $id]);
+        $data = $sth->fetchAll();
+        print_r(json_encode(['data' => $data]));echo "\n\n";exit;
+    } else {
+        // Vulnerable db query
+        echo "Param: " . $id . "\n";
+        $vulnerableSql = "SELECT * FROM customers WHERE id = $id";
+        foreach ($dbh->query($vulnerableSql) as $row) {
+            echo "Name: " . $row['name'] . "\n";
+        }
     }
 }catch(\Exception $e){
     echo $e->getMessage();
