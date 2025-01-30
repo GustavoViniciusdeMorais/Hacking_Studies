abccba5ae56b0c93353233776cc7a8de284277bf
GustavoViniciusdeMorais <gustavocop14dos@gmail.com>
2025-01-30 11:07:37 -0300

    - Improved mysql bash

--- bash/mysql_exec.sh
+++ bash/mysql_exec.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-mysql -u root --password='' --host localhost --port 3306 -e 'use studies; select * from books;'
+# Use in cli as sudo ./mysql_exec.sh
+read -p "Enter the query: " query
+
+password='';
+host='localhost';
+port=3306;
+database='studies';
+
+if [ -n "$query" ]; then
+    printf "Query results:\n";
+    mysql -u root --password=$password --host $host --port $port -e "use $database; $query;"
+else
+    query='select * from books;';
+    mysql -u root --password=$password --host $host --port $port -e "use $database; $query;"
+    printf "No results\n";
+fi
+# mysql -u root --password='' --host localhost --port 3306 -e 'use studies; select * from books;'
