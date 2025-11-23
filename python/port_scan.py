ee9a6dc99d75f4f886f0de6c5d39daed9babeca6
Gustavo <gustavocop14dos@gmail.com>
2025-11-23 11:34:28 -0300

    added port list

--- python/port_scan.py
+++ python/port_scan.py
@@ -1,12 +1,14 @@
 import socket
 import sys
 
+ports = [21,22,80,3306];
+
 if len(sys.argv) < 2:
     ip='10.0.0.3'
 else:
     ip = sys.argv[1]
 
-for port in range(1,65535):
+for port in ports:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket.setdefaulttimeout(1)
 
